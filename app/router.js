84911c05b5ca0957add590588568f9846774cc5c
kaf <kaf@nevod.ru>
2016-02-16 16:42:56 +0500

    Merge

--- app/router.js
+++ app/router.js
@@ -9,16 +9,12 @@ Router.map(function() {
   this.route('suggestionTypes', function() {
     this.route('page', {path: 'page/:page'});
   });
-<<<<<<< HEAD
-  this.route('addSuggestion');
-=======
   this.route('suggestions', function() {
     this.route('page', {path: 'page/:page'});
   });
   this.route('users', function() {
     this.route('page', {path: 'page/:page'});
   });
->>>>>>> e7dfbdcdd04a033ac5202c52c79687e768709d3f
 });
 
 export default Router;
