bed683561e2274056decc32b37d0793331b8248b
kaf <kaf@nevod.ru>
2016-02-17 16:44:27 +0500

    Add  controllers, routes, serializers for suggestions.new. Problem export default EditFormRoute.extend does'nt work - empty page wthout errors

--- app/routes/suggestions/new.js
+++ app/routes/suggestions/new.js
@@ -1,4 +1,19 @@
-import Ember from 'ember';
+// 
+// export default EditFormRoute.extend(AuthenticatedRouteMixin, {
+//   modelProjection: 'SuggestionE',
+//   modelName: 'suggestion'
+// });
+
+// import Ember from 'ember';
+// import EditFormRoute from 'ember-flexberry/routes/edit-form';
+// import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+// // 
+// export default EditFormRoute.extend(AuthenticatedRouteMixin, {
+//   modelProjection: 'SuggestionE',
+//   modelName: 'suggestion'
+// });
 
 export default Ember.Route.extend({
 });
+
+
