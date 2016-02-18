2111647e0f99476d20fc9694351c786fe3a01631
kaf <kaf@nevod.ru>
2016-02-18 14:40:36 +0500

    Delete unused routes, applications (mirage) add .directory to .gitignore

--- app/routes/suggestions/new.js
+++ app/routes/suggestions/new.js
@@ -1,13 +1,8 @@
-// 
-// export default EditFormRoute.extend(AuthenticatedRouteMixin, {
-//   modelProjection: 'SuggestionE',
-//   modelName: 'suggestion'
-// });
-
-// import Ember from 'ember';
+import Ember from 'ember';
 // import EditFormRoute from 'ember-flexberry/routes/edit-form';
-// import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-// // 
+// import AuthenticatedRouteMixin from 'ember-flexberry/ember-simple-auth/mixins/unauthenticated-route-mixin';
+// //                      ember-flexberry/node_modules/ember-simple-auth/addon/mixins/unauthenticated-route-mixin
+// 
 // export default EditFormRoute.extend(AuthenticatedRouteMixin, {
 //   modelProjection: 'SuggestionE',
 //   modelName: 'suggestion'
