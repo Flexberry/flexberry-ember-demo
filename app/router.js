4d7524a6c8e105db3df57459dd4f0d61f5d573f3
kaf <kaf@nevod.ru>
2016-03-11 15:54:23 +0500

    Redesign menu tree

--- app/router.js
+++ app/router.js
@@ -6,22 +6,23 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('flexberry-ember-demo-suggestion-types'); 
   this.route('flexberry-ember-demo-suggestion-type',{path:'flexberry-ember-demo-suggestion-type/:id'}); 
   this.route('flexberry-ember-demo-suggestion-type.new',{path:'flexberry-ember-demo-suggestion-type/new'}); 
+  this.route('flexberry-ember-demo-suggestions');  
+  this.route('flexberry-ember-demo-suggestions.page',{path: 'page/:page'});
   
-//   this.route('flexberry-ember-demo-suggestion-type', function() {
-//     this.route('index',{path:'index/:id'});
-//     this.route('new');
-//   });
-//   this.route('flexberry-ember-demo-suggestion-type.index',{path:'index/:id'});
+  this.route('flexberry-ember-demo-suggestion', {path:'flexberry-ember-demo-suggestions/:id'});
+  this.route('flexberry-ember-demo-suggestion.new', {path:'flexberry-ember-demo-suggestions/new'});
+
 
   this.route('suggestionTypes', function() {
     this.route('page', {path: 'page/:page'});
   });
-  this.route('suggestions', function() {
-    this.route('page', {path: 'page/:page'});
-  });
-  this.route('suggestions.new');
+//   this.route('suggestions', function() {
+//     this.route('page', {path: 'page/:page'});
+//   });
+//   this.route('suggestions.new');
   this.route('users', function() {
     this.route('page', {path: 'page/:page'});
   });
