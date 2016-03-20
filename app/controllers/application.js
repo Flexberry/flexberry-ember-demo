cc3ebaea2e33a9090545a5cce2e9602c34afbb0d
kaf <kaf@nevod.ru>
2016-03-20 06:16:49 +0500

    ember test ucomplete editing:wq

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,41 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-session: Ember.inject.service('session'),
-  sitemap: {
-nodes: [{
-	link: 'index',
-	title: 'Home',
-	children: null
-    }, {
-	link: null,
-	title: 'Objects',
-	children: [{
-	  link: 'flexberry-ember-demo-application-user-l',
-	  title: 'Application user',
-	  children: null
-	  },
-	  {
-	  link: 'flexberry-ember-demo-localization-l',
-	  title: 'Localization',
-	  children: null
-	  },
-	  {
-	  link: 'flexberry-ember-demo-suggestion-l',
-	  title: 'Suggestion',
-	  children: null
-	  },
-	  {
-	  link: 'flexberry-ember-demo-suggestion-type-l',
-	  title: 'Suggestion type',
-	  children: null
-	  }]
-    }]
-},
-  actions: {
-    toggleSidebar: function(direction) {
-      $('.ui.sidebar').sidebar('toggle');
-    }
-  }
-
-}); 
+	session: Ember.inject.service('session'),
+	sitemap: {
+		nodes: [{
+				link: 'index',
+				title: 'Home',
+				children: null
+			}, {
+				link: null,
+				title: 'Objects',
+				children: [{
+						link: 'flexberry-ember-demo-application-user-l',
+						title: 'Application user',
+						children: null
+					},
+					{
+						link: 'flexberry-ember-demo-localization-l',
+						title: 'Localization',
+						children: null
+					},
+					{
+						link: 'flexberry-ember-demo-suggestion-l',
+						title: 'Suggestion',
+						children: null
+					},
+					{
+						link: 'flexberry-ember-demo-suggestion-type-l',
+						title: 'Suggestion type',
+						children: null
+					}
+				]
+			}
+		]
+	},
+	actions: {
+		toggleSidebar: function() {
+			$('.ui.sidebar').sidebar('toggle');
+		}
+	}
+}
+); 
